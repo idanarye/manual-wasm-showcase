3d9520658ae2f3f5e829dc01eb18e7ac28fd5355
Idan Arye <IdanArye@gmail.com>
2024-08-19 22:06:39 +0300

    Actually fix it

--- .idanarye.moonicipal.lua
+++ .idanarye.moonicipal.lua
@@ -2,7 +2,10 @@ local moonicipal = require'moonicipal'
 local T = moonicipal.tasks_file()
 
 function T:run()
-    --require'channelot'.windowed_terminal_job{'./build-missing.nu'}
+    require'channelot'.windowed_terminal_job{'./build-missing.nu'}
+end
+
+function T:act()
     require'channelot'.windowed_terminal_job'./format-index.nu > index.html':wait()
     vim.cmd.checktime()
 end
